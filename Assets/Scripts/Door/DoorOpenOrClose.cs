e335b1f7fbe895c30d8d61ad7ff55b4ae651729e
Rejuanul Huq Sanny <rejuanulhuq@gmail.com>
2026-07-13 02:39:25 +0600

    Door Machanism added and navmesh Fixed, Character Animation added

---
+++ Assets/Scripts/Door/DoorOpenOrClose.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class DoorOpenOrClose : MonoBehaviour
+{
+    [Header("Door Frames")]
+    public Transform doorFrame1;
+    public Transform doorFrame2;
+
+    [Header("Settings")]
+    [SerializeField] private float openAngle = 90f;
+    [SerializeField] private float rotationSpeed = 3f;
+
+    private Quaternion frame1ClosedRotation;
+    private Quaternion frame2ClosedRotation;
+
+    private Quaternion frame1OpenRotation;
+    private Quaternion frame2OpenRotation;
+
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        // Store initial (closed) rotations
+        frame1ClosedRotation = doorFrame1.localRotation;
+        frame2ClosedRotation = doorFrame2.localRotation;
+
+        // Calculate open rotations
+        frame1OpenRotation = frame1ClosedRotation * Quaternion.Euler(0f, openAngle, 0f);
+        frame2OpenRotation = frame2ClosedRotation * Quaternion.Euler(0f, -openAngle, 0f);
+    }
+
+    private void Update()
+    {
+        Quaternion target1 = isOpen ? frame1OpenRotation : frame1ClosedRotation;
+        Quaternion target2 = isOpen ? frame2OpenRotation : frame2ClosedRotation;
+
+        doorFrame1.localRotation = Quaternion.Slerp(
+            doorFrame1.localRotation,
+            target1,
+            rotationSpeed * Time.deltaTime
+        );
+
+        doorFrame2.localRotation = Quaternion.Slerp(
+            doorFrame2.localRotation,
+            target2,
+            rotationSpeed * Time.deltaTime
+        );
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            OpenDoor();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Character"))
+        {
+            CloseDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        isOpen = true;
+    }
+
+    private void CloseDoor()
+    {
+        isOpen = false;
+    }
+}
