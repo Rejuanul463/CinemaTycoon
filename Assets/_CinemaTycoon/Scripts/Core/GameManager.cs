c998e35923ac988947009081b0bedbe994c529af
Rahat Abedin <rahat.abedin@outlook.com>
2026-07-31 08:54:56 +0600

    update

--- Assets/_CinemaTycoon/Scripts/Core/GameManager.cs
+++ Assets/_CinemaTycoon/Scripts/Core/GameManager.cs
@@ -88,7 +88,6 @@ namespace CinemaTycoon.Core
             }
             _instance = this;
             DontDestroyOnLoad(gameObject);
-            PreloadUiAudio();
             ConfigureAudio();
             SceneManager.sceneLoaded += HandleSceneLoaded;
         }
@@ -143,12 +142,6 @@ namespace CinemaTycoon.Core
 
         /// <summary>Plays the shared UI interaction sound, when assigned.</summary>
         public void PlayUiClick() => PlaySfx(uiClickSound);
-
-        private void PreloadUiAudio()
-        {
-            if (uiClickSound != null && uiClickSound.loadState != AudioDataLoadState.Loaded)
-                uiClickSound.LoadAudioData();
-        }
 
         private void ConfigureAudio()
         {
